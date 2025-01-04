1a728e4d9e6fb6493bdc6d4c52d27c81ff462f96
Sam Lee <cable8mm@gmail.com>
2025-01-04 19:23:04 +0900

    Add models

---
+++ app/Models/RegisterOptionGoodRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RegisterOptionGoodRequest extends Model
+{
+    use HasFactory;
+}
