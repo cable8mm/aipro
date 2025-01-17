adedaea1d8de365a8568e65b15b3d71eb41c610d
Sam Lee <cable8mm@gmail.com>
2025-01-18 02:25:58 +0900

    Create authorized traits

---
+++ app/Traits/NovaAuthorizedByNotReviewer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Traits;
+
+use App\Enums\UserType;
+use Illuminate\Http\Request;
+
+/**
+ * Set authorize to Nova resources as an reviewer
+ *
+ * @see https://nova.laravel.com/docs/4.0/resources/authorization.html
+ *
+ * @author Samgu Lee<cable8mm@gmail.com>
+ *
+ * @since 2025. 01. 17.
+ */
+trait NovaAuthorizedByNotReviewer
+{
+    public function authorizedToReplicate(Request $request)
+    {
+        return false;
+    }
+
+    public function authorizedToView(Request $request)
+    {
+        return true;
+    }
+
+    public static function authorizedToCreate(Request $request)
+    {
+        return $request->user()?->type != UserType::REVIEWER->name;
+    }
+
+    public function authorizedToDelete(Request $request)
+    {
+        return $request->user()?->type == UserType::ADMINISTRATOR->name
+            || $request->user()?->type == UserType::DEVELOPER->name;
+    }
+
+    public function authorizedToUpdate(Request $request)
+    {
+        return $request->user()?->type != UserType::REVIEWER->name;
+    }
+}
---
+++ app/Traits/NovaAuthorizedByWarehouser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Traits;
+
+use App\Enums\UserType;
+use Illuminate\Http\Request;
+
+/**
+ * Set authorize to Nova resources as an warehouser
+ *
+ * @see https://nova.laravel.com/docs/4.0/resources/authorization.html
+ *
+ * @author Samgu Lee<cable8mm@gmail.com>
+ *
+ * @since 2025. 01. 17.
+ */
+trait NovaAuthorizedByWarehouser
+{
+    public function authorizedToReplicate(Request $request)
+    {
+        return false;
+    }
+
+    public function authorizedToView(Request $request)
+    {
+        return true;
+    }
+
+    public static function authorizedToCreate(Request $request)
+    {
+        return $request->user()?->type == UserType::ADMINISTRATOR->name
+            || $request->user()?->type == UserType::DEVELOPER->name
+            || $request->user()?->type == UserType::MANAGER->name
+            || $request->user()?->type == UserType::WAREHOUSER->name;
+    }
+
+    public function authorizedToDelete(Request $request)
+    {
+        return false;
+    }
+
+    public function authorizedToUpdate(Request $request)
+    {
+        return $request->user()?->type == UserType::ADMINISTRATOR->name
+            || $request->user()?->type == UserType::DEVELOPER->name
+            || $request->user()?->type == UserType::MANAGER->name
+            || $request->user()?->type == UserType::WAREHOUSER->name;
+    }
+}
