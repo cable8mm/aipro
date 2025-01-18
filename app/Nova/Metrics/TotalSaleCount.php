2d0ee079c8f3e45526dc9baeb5afecbf12b6fb9d
Sam Lee <cable8mm@gmail.com>
2025-01-19 03:00:59 +0900

    Update the main screen

---
+++ app/Nova/Metrics/TotalSaleCount.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use App\Models\OrderShipment;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Value;
+use Laravel\Nova\Nova;
+
+class TotalSaleCount extends Value
+{
+    /**
+     * Calculate the value of the metric.
+     *
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+        return $this->sum($request, OrderShipment::class, 'totalAmount');
+    }
+
+    /**
+     * Get the ranges available for the metric.
+     *
+     * @return array
+     */
+    public function ranges()
+    {
+        return [
+            7 => Nova::__('7 Days'),
+            30 => Nova::__('30 Days'),
+            60 => Nova::__('60 Days'),
+            365 => Nova::__('365 Days'),
+            'TODAY' => Nova::__('Today'),
+            'MTD' => Nova::__('Month To Date'),
+            'QTD' => Nova::__('Quarter To Date'),
+            'YTD' => Nova::__('Year To Date'),
+        ];
+    }
+
+    /**
+     * Determine the amount of time the results of the metric should be cached.
+     *
+     * @return \DateTimeInterface|\DateInterval|float|int|null
+     */
+    public function cacheFor()
+    {
+        return now()->addMinutes(5);
+    }
+
+    public function name()
+    {
+        return __('Total Sale Count');
+    }
+}
