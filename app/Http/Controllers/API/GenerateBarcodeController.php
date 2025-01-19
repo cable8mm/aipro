51dc3feae1e5bef590280ab1412d1e38bf92302e
Sam Lee <cable8mm@gmail.com>
2025-01-20 00:26:44 +0900

    Fix a bug of the png header

--- app/Http/Controllers/API/GenerateBarcodeController.php
+++ app/Http/Controllers/API/GenerateBarcodeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Support\Barcode;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class GenerateBarcodeController extends Controller
 {
@@ -12,6 +13,10 @@ class GenerateBarcodeController extends Controller
     {
         $number = $request->get('number');
 
-        return Barcode::factory($number)->render();
+        return Response::make(
+            Barcode::factory($number)->render(),
+            200,
+            ['Content-Type' => 'image/png']
+        );
     }
 }
