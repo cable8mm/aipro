d59b8f7b95e9dbfa7a2af3f18e5551779ed6d799
Sam Lee <cable8mm@gmail.com>
2025-02-08 19:57:22 +0900

    Remove seed order sheet excel file

--- database/seeders/OrderSheetInvoiceSeeder.php
+++ database/seeders/OrderSheetInvoiceSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Cable8mm\OrderSheet\Enums\OrderSheetType;
+use Cable8mm\OrderSheet\OrderSheet;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
@@ -20,10 +22,11 @@ class OrderSheetInvoiceSeeder extends Seeder
             storage_path('app/private/invoices/order_sheet_invoice.pdf')
         );
 
-        File::copy(
-            base_path('tests/files/invoices/order_sheet_invoice_simple.xlsx'),
-            storage_path('app/private/invoices/order_sheet_invoice_simple.xlsx')
-        );
+        OrderSheet::of(OrderSheetType::PlayautoType)
+            ->count(10)
+            ->header()
+            ->path(storage_path('app/private/invoices'))
+            ->xlsx('order_sheet_invoice_simple.xlsx');
 
         \App\Models\OrderSheetInvoice::factory()->count(10)->create();
     }
