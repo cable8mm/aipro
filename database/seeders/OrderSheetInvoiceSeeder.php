8a89010396388600012656ecfa54274cfdf68723
Samgu Lee <cable8mm@gmail.com>
2025-02-12 22:16:17 +0900

    Remove seed order sheet excel file (#48)
    * Remove seed order sheet excel file
    
    * Provide waybill pdf seed

--- database/seeders/OrderSheetInvoiceSeeder.php
+++ database/seeders/OrderSheetInvoiceSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use Cable8mm\OrderSheet\Enums\OrderSheetType;
+use Cable8mm\OrderSheet\OrderSheet;
+use Cable8mm\Waybill\Enums\ParcelService;
+use Cable8mm\Waybill\Waybill;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class OrderSheetInvoiceSeeder extends Seeder
@@ -15,15 +18,15 @@ class OrderSheetInvoiceSeeder extends Seeder
     {
         Storage::disk('local')->makeDirectory('invoices');
 
-        File::copy(
-            base_path('tests/files/invoices/order_sheet_invoice.pdf'),
-            storage_path('app/private/invoices/order_sheet_invoice.pdf')
-        );
+        Waybill::of(ParcelService::Cj)
+            ->path(storage_path('app/private/invoices'))
+            ->save('order_sheet_invoice.pdf');
 
-        File::copy(
-            base_path('tests/files/invoices/order_sheet_invoice_simple.xlsx'),
-            storage_path('app/private/invoices/order_sheet_invoice_simple.xlsx')
-        );
+        OrderSheet::of(OrderSheetType::PlayautoType)
+            ->count(10)
+            ->header()
+            ->path(storage_path('app/private/invoices'))
+            ->xlsx('order_sheet_invoice_simple.xlsx');
 
         \App\Models\OrderSheetInvoice::factory()->count(10)->create();
     }
