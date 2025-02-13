103decefeeea31502f0d4efdc2cbb9e254e8660d
Sam Lee <cable8mm@gmail.com>
2025-02-13 17:47:19 +0900

    Add the apis for warehousing

--- app/Http/Controllers/API/GoodController.php
+++ app/Http/Controllers/API/GoodController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\GoodResource;
 use App\Models\Good;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GoodController extends Controller
 {
@@ -13,7 +16,7 @@ class GoodController extends Controller
      *
      * Get goods with paging
      */
-    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    public function index()
     {
         $items = Good::latest()->paginate(request('count', 12));
 
@@ -21,36 +24,123 @@ class GoodController extends Controller
     }
 
     /**
-     * /good/{id}
+     * /good/{good}
      *
      * Get a good
      */
-    public function show(Good $good): \Illuminate\Http\Resources\Json\JsonResource
+    public function show(Good $good)
     {
         return new GoodResource($good);
     }
 
     /**
-     * /good/{master_code}
+     * /good/{masterCode}
      *
      * Get a good by master code
      */
-    public function showByMasterCode(string $showByMasterCode): \Illuminate\Http\Resources\Json\JsonResource
+    public function showByMasterCode(string $masterCode)
     {
-        $good = Good::where('master_code', $showByMasterCode)->firstOrFail();
+        Validator::make(
+            ['masterCode' => $masterCode],
+            ['masterCode' => 'required|string']
+        );
+
+        try {
+            $good = Good::where('master_code', $masterCode)->firstOrFail();
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => __('The given barcode was invalid.'),
+            ], 400);
+        }
 
         return new GoodResource($good);
     }
 
     /**
-     * /good/{barcode}
+     * /good/barcode/{barcode}
      *
      * Get a good by barcode
      */
-    public function showByBarcode(string $barcode): \Illuminate\Http\Resources\Json\JsonResource
+    public function showByBarcode(Request $request, Good $good, string $barcode)
     {
-        $good = Good::where('barcode', $barcode)->firstOrFail();
+        Validator::make(
+            ['barcode' => $barcode],
+            ['barcode' => 'required|string']
+        );
+
+        $validator = Validator::make($request->all(), [
+            'barcode' => 'required|integer|not_in:0',
+        ]);
+
+        $validated = $validator->validated();
+
+        try {
+            $good = $good->where('barcode', $barcode)->firstOrFail();
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => __('The given barcode was invalid.'),
+            ], 400);
+        }
 
         return new GoodResource($good);
+    }
+
+    /**
+     * /good/{good}/balance
+     *
+     * Add or subtract a inventory amount
+     *
+     * @param  \App\Models\Good  $good  The good model
+     * @return GoodResource|\Illuminate\Http\JsonResponse The good resource if successful, otherwise returns a json response
+     */
+    public function balance(Request $request, Good $good)
+    {
+        $validated = $request->validate([
+            'amount' => 'required|integer|not_in:0',
+        ]);
+
+        if ($good->inventory($validated['amount'])) {
+            return new GoodResource($good);
+        }
+
+        return response()->json([
+            'message' => __('Internal Server Error.'),
+        ], 500);
+    }
+
+    /**
+     * /good/{good}/balance-by-barcode
+     *
+     * Add or subtract a inventory amount
+     *
+     * @param  string  $barcode  The barcode of a good
+     * @return GoodResource|\Illuminate\Http\JsonResponse The good resource if successful, otherwise returns a json response
+     */
+    public function balanceByBarcode(Request $request, string $barcode)
+    {
+        Validator::make(
+            ['barcode' => $barcode],
+            ['barcode' => 'required|string']
+        );
+
+        $validated = $request->validate([
+            'amount' => 'required|integer|not_in:0',
+        ]);
+
+        try {
+            $good = Good::where('barcode', $barcode)->firstOrFail();
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => __('The given barcode was invalid.'),
+            ], 400);
+        }
+
+        if ($good->inventory($validated['amount'])) {
+            return new GoodResource($good);
+        }
+
+        return response()->json([
+            'message' => __('Internal Server Error.'),
+        ], 500);
     }
 }
