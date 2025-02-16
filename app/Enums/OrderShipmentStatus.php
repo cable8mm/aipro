212603eb5461abce31569c5584b2e021c8272533
Sam Lee <cable8mm@gmail.com>
2025-02-17 01:02:31 +0900

    Update Enums

--- app/Enums/OrderShipmentStatus.php
+++ app/Enums/OrderShipmentStatus.php
@@ -11,11 +11,13 @@ enum OrderShipmentStatus: string
     case 송장입력 = '송장입력';
     case 상품준비중 = '상품준비중';
     case 검수완료 = '검수완료';
+    case 부분검수완료 = '부분검수완료';
+    case 임시저장 = '임시저장';
     case 에러 = '에러';
 
     public static function loadingWhen(): array
     {
-        return [self::송장입력->name, self::상품준비중->name];
+        return [self::송장입력->name, self::상품준비중->name, self::부분검수완료, self::임시저장];
     }
 
     public static function failedWhen(): array
