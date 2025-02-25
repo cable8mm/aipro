5707a1cbc0fe98fc1977b2bfc89828ccbc248e9e
Sam Lee <cable8mm@gmail.com>
2025-02-25 12:50:01 +0900

    Update assets

--- database/migrations/2025_01_05_063245_create_boxes_table.php
+++ database/migrations/2025_01_05_063245_create_boxes_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class, 'author_id')->constrained()->comment('작성자 아이디');
             $table->string('name', 100)->comment('박스 이름');
-            $table->string('code', 50)->comment('박스 코드');
+            $table->string('sku', 50)->comment('박스 SKU');
             $table->unsignedInteger('size')->comment('박스 사이즈');
             $table->integer('delivery_price')->nullable()->comment('배송 금액');
-            $table->integer('cost_price')->nullable()->comment('매입가');
+            $table->integer('purchase_price')->nullable()->comment('매입가');
             $table->bigInteger('inventory')->default(0)->comment('재고 수량');
             $table->string('memo', 255)->nullable()->comment('메모');
             $table->boolean('is_default')->default(false)->comment('기본 박스 여부, 주문서에 별도의 박스 규격이 없다면 이 박스를 사용');
