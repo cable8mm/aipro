dbe74cb496ebf98c2e476dc60683b9accc34271a
Sam Lee <cable8mm@gmail.com>
2025-02-28 19:12:26 +0900

    Remove `Channel` and `ChannelFee` resources and related files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,6 @@ class DatabaseSeeder extends Seeder
             AlertEmailSeeder::class,
             BoxManualWarehousingSeeder::class,
             BoxPurchaseOrderSeeder::class,
-            ChannelFeeSeeder::class,
-            ChannelSeeder::class,
             HelpTipSeeder::class,
             MismatchedOrderShipmentSeeder::class,
             NaverCategorySeeder::class,
