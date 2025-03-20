b781e10e023dda87828facf234cfa3c469ea8c67
Sam Lee <cable8mm@gmail.com>
2025-03-21 01:15:25 +0900

    Add `retail-purchases.print`

---
+++ app/Nova/Actions/PrintRetailPurchase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Actions\ActionResponse;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class PrintRetailPurchase extends Action
+{
+    use InteractsWithQueue;
+    use Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        foreach ($models as $model) {
+            return ActionResponse::openInNewTab(route('retail-purchases.print', ['retailPurchase' => $model->id]));
+        }
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array<int, \Laravel\Nova\Fields\Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    public function name()
+    {
+        return __('Print Retail Purchase');
+    }
+}
